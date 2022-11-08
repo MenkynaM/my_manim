97e274dfa9a3ee372fea998983ee45258d1058b6
Martin Menkyna <menkyna.m@gmail.com>
2022-11-08 15:57:33 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,46 @@
+from manim import *
+
+class Precession(ThreeDScene):
+	def create_spin(self, phase: float, cone: Circle):
+		spin = Arrow3D(start=[0, 0, 0], end=[cone.radius * np.cos(phase), cone.radius * np.sin(phase), cone.get_z()])
+		return spin
+
+	def create_more_spins(self, n: int, cone):
+		# angles = [360. / i for i in range(n)]
+		return VGroup(*[self.create_spin(i * 360 * DEGREES / n, cone=cone) for i in range(n)])
+
+
+	def construct(self):
+		cone_top = Circle(radius=1)
+		cone_bottom = Circle(radius=1)
+
+		cone_top.shift(2 * OUT)
+		cone_bottom.shift(2 * IN)
+
+		self.cone_top = cone_top
+		self.cone_bottom = cone_bottom
+
+		
+
+		spins_top = self.create_more_spins(3, self.cone_top)
+		self.add(spins_top)
+		self.add(cone_top)
+
+
+		spins_bottom = self.create_more_spins(3, self.cone_bottom)
+		self.add(spins_bottom)
+		self.add(cone_bottom)
+
+
+
+		self.set_camera_orientation(phi=70 * DEGREES)
+
+
+		self.play(
+			Rotate(spins_top, angle=2 * PI, about_point=[0, 0, 1]),
+			Rotate(spins_bottom, angle=2 * PI, about_point=[0, 0, 1]),
+			run_time=2
+		)
+		self.move_camera(phi=0)
+		self.wait()
+		# self.play(Rotate(spins_bottom, angle=2 * PI, about_point=[0, 0, 1]), run_time=4)
